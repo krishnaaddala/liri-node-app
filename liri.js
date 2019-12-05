9db0705f1a59b3b74712d7083f660db39284a590
Krishna <kaddala@salesforce.com>
2019-12-05 00:13:31 -0800

    adding final logic for liri.js

--- liri.js
+++ liri.js
@@ -9,113 +9,46 @@ var spotify = new Spotify(keys.spotify);
 var commandInput = process.argv[2];
 var searchInput = process.argv[3];
 
-// Make it so liri.js can take in one of the following commands:
-
-// ***concert-this
-// This will search the Bands in Town Artist Events API ("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp") for an artist and render the following information about each event to the terminal:
-// Name of the venue
-// Venue location
-// Date of the Event (use moment to format this as "MM/DD/YYYY")
-
+//concert-this
 if (commandInput === "concert-this"){
     console.log("***********************searching for a concert***********************")
     bandsInTown(searchInput)
 }
-else{
-    bandsInTown("Hello");
-    // console.log("Choose a band")
-}
-// ***spotify-this-song
-// node liri.js spotify-this-song '<song name here>'
-if (commandInput === "spotify-this-song"){
-// This will show the following information about the song in your terminal/bash window
-// Artist(s)
-// The song's name
-// A preview link of the song from Spotify
-// The album that the song is from
-// If no song is provided then your program will default to "The Sign" by Ace of Base.
-// You will utilize the node-spotify-api package in order to retrieve song information from the Spotify API.
-// The Spotify API requires you sign up as a developer to generate the necessary credentials. You can follow these steps in order to generate a client id and client secret:
+//spotify-this
+else if (commandInput === "spotify-this-song"){
     console.log("*********************searching for a song on spotify*********************")
     spotifySongs(searchInput);
 }
-else{
-    spotifySongs("I want it that way");
-}
+
 //movie-this
-if (commandInput === "movie-this"){
+else if (commandInput === "movie-this"){
     console.log("****************************searching for a movie on OMDB********************")
     moviesDB(searchInput);
 }
-else{
-    moviesDB("Matrix");
-
-}
-// do-what-it-says
-// node liri.js do-what-it-says
-
-// Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-
-// It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
-
-// Edit the text in random.txt to test out the feature for movie-this and concert-this.
-
-if (commandInput === "do-what-it-says"){
-   // console.log("searching for a random text inserted")
+//read from random.txt
+else if (commandInput === "do-what-it-says"){
+   console.log("******************************searching for a random.txt file**********************")
    readFromFS();
-
-
-//put a ifElse into a function 
-//function do this takes 2 args (command & search term)
-// call the function again to pass arr[0] & arr [1]
 }
 
-// else{
-//     console.log("LIRI does not know what to do")
-// }
 
-
-// node liri.js movie-this '<movie name here>'
-
-
-// 
-
-
-// If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-
-
-// If you haven't watched "Mr. Nobody," then you should: http://www.imdb.com/title/tt0485947/
-
-
-// It's on Netflix!
-
-
-
-
-// You'll use the axios package to retrieve data from the OMDB API. Like all of the in-class activities, the OMDB API requires an API key. You may use trilogy.
 function bandsInTown(band){
-var queryUrl = "https://rest.bandsintown.com/artists/" + searchInput + "/events?app_id=codingbootcamp";
-    axios.get(queryUrl).then(function(response){
-    console.log(response.data);
-
+var bandsUrl = "https://rest.bandsintown.com/artists/" + searchInput + "/events?app_id=codingbootcamp";
+    axios.get(bandsUrl).then(function(response){
+        for (i=0; i < response.data.length; i++){
+    console.log(response.data[i].venue.name + " " +response.data[i].venue.city + " " +response.data[i].datetime);
+        }
     });
 }
 //blueprint from spotifyAPI
 function spotifySongs(songName){
     spotify.search({ type: 'track', query: searchInput, limit: 1}, function(error, data) {
         if (!error) {
-            console.log("Artist : " + data.tracks.items.album.artists[3].name); 
+                    for (var i = 0; i < data.tracks.items.length; i++) {
+                        var searchSong = data.tracks.items[i];
+                        console.log("Artist : " + searchSong.artists[i].name + searchSong.name + searchSong.preview_url + searchSong.album.name); 
 
-
-            // var searchWordsSpaced  = function () {
-            //         for (var i = 0; i < data.tracks.items.length; i++) {
-            //             var searchSong = data.tracks.items[i];
-            //             console.log("Artist : " + searchSong.artists[0].name); 
-            //             // console.log(data);
-
-            //         }
-            //         // console.log(searchSpaced);
-            //     };
+                    }
         }
       else{
         return console.log('Error occurred: ' + error);
@@ -131,6 +64,8 @@ function spotifySongs(songName){
 //   * Language of the movie.
 //   * Plot of the movie.
 //   * Actors in the movie.
+// You'll use the axios package to retrieve data from the OMDB API. Like all of the in-class activities, the OMDB API requires an API key. You may use trilogy.
+
 function moviesDB(movie){
     var queryUrl = "http://www.omdbapi.com/?t=" + searchInput + "&y=&plot=short&apikey=trilogy";
 
@@ -144,8 +79,9 @@ function moviesDB(movie){
     console.log("Language: " + response.data.Language);
     console.log("Plot: " + response.data.Plot);
     console.log("Actors: " + response.data.Actors);
-  })
+  });
 }
+
 function readFromFS(){
 fs.readFile("random.txt", "utf8", function(err,data){
 //console.log(data);
